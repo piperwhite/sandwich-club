e7b5e4379b218d590bc502e8d86e04c5ab5c4660
Blanca Gomez Andrades <blancagomez@Blancas-MBP.hsd1.ca.comcast.net>
2019-11-25 15:18:04 -0800

    populate to UI

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,17 +3,29 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private TextView originTv;
+    private ListView alsoKnownLv;
+    private ListView ingredientsLv;
+    private TextView descriptionTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +33,10 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        originTv = findViewById(R.id.origin_tv);
+        alsoKnownLv = findViewById(R.id.also_known_lv);
+        ingredientsLv = findViewById(R.id.ingredients_lv);
+        descriptionTv = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +59,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +72,14 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
-
+    private void populateUI(Sandwich sandwich) {
+        originTv.setText(sandwich.getPlaceOfOrigin());
+        descriptionTv.setText(sandwich.getDescription());
+        ArrayAdapter<String> alsoKnownAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sandwich.getAlsoKnownAs());
+        alsoKnownLv.setAdapter(alsoKnownAdapter);
+        ArrayAdapter<String> ingredientsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sandwich.getIngredients());
+        ingredientsLv.setAdapter(ingredientsAdapter);
     }
 }
